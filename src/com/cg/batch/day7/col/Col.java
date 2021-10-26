7350fbfeb3a73d4e68267711fabcd7bdd1864eea
Ranjith <ursranjith18@gmail.com>
2021-10-26 10:08:32 +0530

    Update Col.java

--- src/com/cg/batch/day7/col/Col.java
+++ src/com/cg/batch/day7/col/Col.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
 
 	ArrayList myList = new ArrayList();
 	System.out.println(myList);
-	System.out.println(myList.size());
+	System.out.println(myList.size());  
 	myList.add(10);
 	myList.add(10.5);
 	myList.add("abc");
